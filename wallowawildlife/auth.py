dd05cd38f7654cd0384a420f731e88a5bc965a35
Jenner Hanni <jenner@wickerbox.net>
2018-07-28 09:54:06 -0700

    feat: Add static style, fix registration

--- wallowawildlife/auth.py
+++ wallowawildlife/auth.py
@@ -13,24 +13,24 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
   if request.method == 'POST':
-    username = request.form['username']
+    name = request.form['name']
     password = request.form['password']
     db = get_db()
     error = None
 
-    if not username:
+    if not name:
       error = 'Username is required.'
     elif not password:
       error = 'Password is required.'
     elif db.execute(
-      'SELECT id FROM user WHERE username = ?', (username,)
+      'SELECT id FROM user WHERE name = ?', (name,)
     ).fetchone() is not None:
-      error = 'User {} is already registered.'.format(username)
+      error = 'User {} is already registered.'.format(name)
 
     if error is None:
       db.execute(
-        'INSERT INTO user (username, password) VALUES (?, ?)',
-        (username, generate_password_hash(password))
+        'INSERT INTO user (name, password) VALUES (?, ?)',
+        (name, generate_password_hash(password))
       )
       db.commit()
       return redirect(url_for('auth.login'))
@@ -43,16 +43,16 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
   if request.method == 'POST':
-    username = request.form['username']
+    name = request.form['name']
     password = request.form['password']
     db = get_db()
     error = None
     user = db.execute(
-      'SELECT * FROM user WHERE username = ?', (username,)
+      'SELECT * FROM user WHERE name = ?', (name,)
     ).fetchone()
 
     if user is None:
-      error = 'Incorrect username.'
+      error = 'Incorrect name.'
     elif not check_password_hash(user['password'], password):
       error = 'Incorrect password.'
 
