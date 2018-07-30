cb01be9594976d1b668da60cfa81e2d7f2ccd052
Jenner Hanni <jenner@wickerbox.net>
2018-07-30 11:49:34 -0700

    feat: Add deleteCreature route and template

--- wallowawildlife/lists.py
+++ wallowawildlife/lists.py
@@ -78,3 +78,13 @@ def editCreature(creature_id):
 
   return render_template('/lists/creature_edit.html', types=types,
                          creature=creature)
+
+@bp.route('/wildlife/<int:creature_id>/delete')
+def deleteCreature(creature_id):
+  db = get_db()
+  types = db.execute('SELECT * FROM creature_type').fetchall()
+  creature = db.execute('SELECT * FROM creature WHERE id = ?',
+                         (creature_id,)).fetchone()
+
+  return render_template('/lists/creature_delete.html', types=types,
+                         creature=creature)
