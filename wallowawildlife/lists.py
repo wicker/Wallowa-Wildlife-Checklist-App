c31fd872198d5b52bb417d64521dcdfcb2f4f671
Jenner Hanni <jenner@wickerbox.net>
2018-07-30 10:54:56 -0700

    feat: Set up routing for type pages

--- wallowawildlife/lists.py
+++ wallowawildlife/lists.py
@@ -19,10 +19,35 @@ from wallowawildlife.db import get_db
 
 bp = Blueprint('lists', __name__)
 
-@bp.route('/all')
-def list_all():
+@bp.route('/wildlife')
+def listAll():
   db = get_db()
   types = db.execute('SELECT * FROM creature_type').fetchall()
   creatures = db.execute('SELECT * FROM creature').fetchall()
   return render_template('lists/list_all.html', types=types, creatures=creatures)
+
+@bp.route('/wildlife/<url_text>')
+def listByType(url_text):
+  db = get_db()
+  types = db.execute('SELECT * FROM creature_type').fetchall()
+  creatures = db.execute('SELECT * FROM creature').fetchall()
+  creaturesDisplayable = []
+
+  if url_text == 'spider_insect':
+    title = 'Spiders/Insects'
+  elif url_text == 'reptile_amphibian':
+    title = 'Reptiles/Amphibians'
+  elif url_text == 'tree_shrub':
+    title = 'Tree/Shrub'
+  elif url_text == 'fish':
+    title = 'Fishes'
+  else:
+    title = url_text.capitalize() + 's'
+
+  for c in creatures:
+    if (c['type_id'] == url_text):
+      creaturesDisplayable.append(c)
+
+  return render_template('lists/list_by_type.html', types=types,
+      creatures=creaturesDisplayable, page_title=title)
 
